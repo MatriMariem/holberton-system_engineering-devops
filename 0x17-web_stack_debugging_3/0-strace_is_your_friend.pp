5662337795cc0d263afd00ab9b9cef8305ab6120
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-05-19 18:38:58 +0100

    fix comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
--- web stack debugging by fixing a line in configuration file
+# web stack debugging by fixing a line in configuration file
 exec {'config file':
 path     => ['/usr/bin', '/bin'],
 command  => "sudo sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
