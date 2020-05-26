181a9b08618ce07873fdce5061a2a7f9beb6742b
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-05-26 20:23:44 +0100

    set ULIMIT TO 3000

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
 # increase number of open fd
-exec { 'fd':
-  command =>'sudo sed -i \'s/15/3000/g\' etc/nginx/nginx.conf; sudo service ngix restart',
-  path    =>['/bin'],
+exec {'fd':
+path     => ['/usr/bin', '/bin'],
+command  => "sudo sed -i 's/15/3000/g' /etc/default/nginx; sudo service nginx restart",
+provider => 'shell',
 }
